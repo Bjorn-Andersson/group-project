921e0f4d6e1e6b1fa8644680b1b78f1b223d9b2d
Emelie Wensberg <emelie.wensberg@iths.se>
2022-05-03 20:35:31 +0200

    merge

--- public/main.js
+++ public/main.js
@@ -25,6 +25,14 @@ fetch(url)
     console.log(error)
   })
 
+  $(document).ready(function(){
+    $('input.typeahead').typeahead({
+    name: 'typeahead',
+    remote: 'http://localhost:3000/search?key=%QUERY',
+    limit: 10
+    });
+    });
+
 const searchInput = document.getElementById("searchInput");
 
 const storeSingers = document.getElementsByClassName("singers");
